9068483b85d0af6163f0fc3e3885d0cb7f948746
cmerck1 <85362441+cmerck1@users.noreply.github.com>
2021-07-26 16:03:27 -0400

    Update ExcerciseLibrary.py

--- ExcerciseLibrary.py
+++ ExcerciseLibrary.py
@@ -1,33 +1,33 @@
-def chest():
+class chest():
     chest = []
     barbell = ["Bench Press", "Incline Bench Press"]
     dumbell = ["Dumbell Bench Press", "Dumbell Incline Bench Press", "Dumbell Flys"]
     calisthenics = ["pushups", "decline pushups"]
 
-def bices():
+class bices():
     biceps = []
     barbell = ["Barbell Curls", "Curl Bar Preacher Curls"]
     dumbell = ["Standing Dumbell Curls", "Hammer Curls", "Dumbell Preacher Curls"]
     calisthenics = ["Chin-Ups"]
 
-def back():
+class back():
     back = []
     barbell = ["Bent over barbell row", "supenated grip barbell row"]
     dumbell = ["bent over dumbell row", "Single arm dumbell row", ]
     calisthenics = ["Wide Grip Pullup"]
 
-def shoulders():
+
+class shoulders():
     shoulders = []
     barbell = ["Standing military press"]
     dumbell = ["Dumbell military press", "Dumbell Lateral Raises", "Dumbell Front Raises"]
     calisthenics = ["decline pushups"]
 
-def legs():
+class legs():
     legs = []
     barbell = ["Back Squat", "Barbell lunges"]
     dumbell = ["Golblet squat", "dumbell walking lunges"]
     calisthenics = ["Bodyweight Squat", "Bodyweight lunge"]
-
     
 
 
