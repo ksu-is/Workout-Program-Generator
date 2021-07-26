b5b13eb39f747a01a72010f577ace20d89e3f7a8
cmerck1 <85362441+cmerck1@users.noreply.github.com>
2021-07-26 16:19:45 -0400

    Update MyWorkoutGenerator.py
    updated code

--- MyWorkoutGenerator.py
+++ MyWorkoutGenerator.py
@@ -3,3 +3,8 @@ from ExcerciseLibrary import *
 
 
 question = input("What is your goal for today's workout? (Build Muscle, Gain Strength, or Lose Weight)\n ")
+
+if question == "Build Muscle".lower():
+    muscle_group = input("What muscle group would you like to worjout today? (Chest, biceps, back, legs, or shoulders)")
+    if muscle_group == "chest".lower():
+        print(chest.barbell)
