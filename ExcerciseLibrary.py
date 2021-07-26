1e8aae63a195f7e30814822c8a0a504327ae32ca
cmerck1 <85362441+cmerck1@users.noreply.github.com>
2021-07-26 15:54:36 -0400

    Add files via upload

---
+++ ExcerciseLibrary.py
@@ -0,0 +1,33 @@
+def chest():
+    chest = []
+    barbell = ["Bench Press", "Incline Bench Press"]
+    dumbell = ["Dumbell Bench Press", "Dumbell Incline Bench Press", "Dumbell Flys"]
+    calisthenics = ["pushups", "decline pushups"]
+
+def bices():
+    biceps = []
+    barbell = ["Barbell Curls", "Curl Bar Preacher Curls"]
+    dumbell = ["Standing Dumbell Curls", "Hammer Curls", "Dumbell Preacher Curls"]
+    calisthenics = ["Chin-Ups"]
+
+def back():
+    back = []
+    barbell = ["Bent over barbell row", "supenated grip barbell row"]
+    dumbell = ["bent over dumbell row", "Single arm dumbell row", ]
+    calisthenics = ["Wide Grip Pullup"]
+
+def shoulders():
+    shoulders = []
+    barbell = ["Standing military press"]
+    dumbell = ["Dumbell military press", "Dumbell Lateral Raises", "Dumbell Front Raises"]
+    calisthenics = ["decline pushups"]
+
+def legs():
+    legs = []
+    barbell = ["Back Squat", "Barbell lunges"]
+    dumbell = ["Golblet squat", "dumbell walking lunges"]
+    calisthenics = ["Bodyweight Squat", "Bodyweight lunge"]
+
+    
+
+
---
+++ MyWorkoutGenerator.py
@@ -0,0 +1,5 @@
+from ExcerciseLibrary import *
+
+
+
+question = input("What is your goal for today's workout? (Build Muscle, Gain Strength, or Lose Weight)\n ")
